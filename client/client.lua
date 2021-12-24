e1de7e483e8371ac6449e8fba42a40502eb64f7c
uyuyorum {um} <56808589+alp1x@users.noreply.github.com>
2021-12-24 21:49:49 +0300

    chat fixed

--- client/client.lua
+++ client/client.lua
@@ -14,8 +14,25 @@ CreateThread(function()
         local sleep = 500 
         if UMHackerPhoneStatus then
         sleep = 5 
-        DisableControlAction(0, 24, true) 
-        DisableControlAction(0, 257, true) 
+	    DisableControlAction(0, 1, true) -- disable mouse look
+	    DisableControlAction(0, 2, true) -- disable mouse look
+	    DisableControlAction(0, 3, true) -- disable mouse look
+	    DisableControlAction(0, 4, true) -- disable mouse look
+	    DisableControlAction(0, 5, true) -- disable mouse look
+	    DisableControlAction(0, 6, true) -- disable mouse look
+	    DisableControlAction(0, 263, true) -- disable melee
+	    DisableControlAction(0, 264, true) -- disable melee
+	    DisableControlAction(0, 257, true) -- disable melee
+	    DisableControlAction(0, 140, true) -- disable melee
+	    DisableControlAction(0, 141, true) -- disable melee
+	    DisableControlAction(0, 142, true) -- disable melee
+	    DisableControlAction(0, 143, true) -- disable melee
+	    DisableControlAction(0, 177, true) -- disable escape
+	    DisableControlAction(0, 200, true) -- disable escape
+	    DisableControlAction(0, 202, true) -- disable escape
+	    DisableControlAction(0, 322, true) -- disable escape
+	    DisableControlAction(0, 245, true) -- disable chat
+	    DisableControlAction(0, 24, true) 
         end
         Wait(sleep)
     end
