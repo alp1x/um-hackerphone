b54d43bdb2c9b01024dff51ecdcca2a4b643acd4
uyuyorum {um} <56808589+alp1x@users.noreply.github.com>
2022-09-15 17:28:54 +0300

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -9,23 +9,23 @@ RegisterNetEvent('um-hackerphone:server:targetinformation', function()
    local PlayerPed = GetPlayerPed(src)
    local pCoords = GetEntityCoords(PlayerPed)
    local found = false
-   for k, v in pairs(QBCore.Functions.GetPlayers()) do
-      local TargetPed = GetPlayerPed(v)
-      local tCoords = GetEntityCoords(TargetPed)
-      local dist = #(pCoords - tCoords)
-      if PlayerPed ~= TargetPed and dist < 3.0 then
-         found = true
-         TargetPlayer = QBCore.Functions.GetPlayer(v)
-      end
-  end
+      for k, v in pairs(QBCore.Functions.GetPlayers()) do
+         local TargetPed = GetPlayerPed(v)
+         local tCoords = GetEntityCoords(TargetPed)
+         local dist = #(pCoords - tCoords)
+         if PlayerPed ~= TargetPed and dist < 3.0 then
+            found = true
+            TargetPlayer = QBCore.Functions.GetPlayer(v)
+         end
+     end
   if found then 
-   local targetinfo = {
-      ['targetname'] = TargetPlayer.PlayerData.charinfo.firstname,
-      ['targetlastname'] = TargetPlayer.PlayerData.charinfo.lastname,
-      ['targetdob'] = TargetPlayer.PlayerData.charinfo.birthdate,
-      ['targetphone'] = TargetPlayer.PlayerData.charinfo.phone,
-      ['targetbank'] = TargetPlayer.PlayerData.money['bank']
-   }
+         local targetinfo = {
+            ['targetname'] = TargetPlayer.PlayerData.charinfo.firstname,
+            ['targetlastname'] = TargetPlayer.PlayerData.charinfo.lastname,
+            ['targetdob'] = TargetPlayer.PlayerData.charinfo.birthdate,
+            ['targetphone'] = TargetPlayer.PlayerData.charinfo.phone,
+            ['targetbank'] = TargetPlayer.PlayerData.money['bank']
+         }
       TriggerClientEvent('um-hackerphone:client:targetinfornui',src,targetinfo)
    else
       TriggerClientEvent('um-hackerphone:client:notify',src)
